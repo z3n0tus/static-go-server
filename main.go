e62ddd17c8b988865cc462b1ed0819455f5b97c9
Lee <lee.mcgowan.work@gmail.com>
2020-05-23 19:47:26 +0100

    Finished code for static site

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+	"encoding/json"
+	"fmt"
+)
+
+type author struct {
+	Name string
+	FavouriteBook string
+}
+
+var favouriteAuthors [6]author = [6]author{
+	{ Name: "N. K. Jemisin", FavouriteBook: "The Fifth Season" },
+	{ Name: "Noam Chomsky", FavouriteBook: "Who Rules the World?" },
+	{ Name: "Nnedi Okorafor", FavouriteBook: "Who Fears Death?" },
+	{ Name: "Robert Jordan", FavouriteBook: "The Dragon Reborn" },
+	{ Name: "Frederick Forsyth", FavouriteBook: "The Day of the Jackal" },
+	{ Name: "Octavia Butler", FavouriteBook: "Kindred" },
+}
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/*", fs)
+
+	http.HandleFunc("/", spaHandler);
+
+	http.HandleFunc("/authors", authorHandler);
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func authorHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	favouriteAuthorsJSON, err := json.Marshal(favouriteAuthors)
+
+	if err != nil {
+		panic("Could not marshal json.")
+	}
+
+	fmt.Fprintf(responseWriter, string(favouriteAuthorsJSON))
+}
+
+func spaHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	http.ServeFile(responseWriter, request, "./static/index.html")
+}
